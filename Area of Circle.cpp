fe8f435b51f89f1ff5cf1b7d51b2dfa6e4c3502a
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2024-12-29 00:58:36 +0600

    Create Area of Circle.cpp

---
+++ Area of Circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    float r, pi=3.14,area;
+    cout<<"Enter the radius: ";
+    cin>>r;
+
+    area=pi*r*r;
+    cout<<showpoint;
+    cout<<setprecision(4);
+    cout<<"The area of circle is: "<<area;
+    getch();
+}
