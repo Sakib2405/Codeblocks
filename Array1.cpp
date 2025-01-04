653fd3227b0ccde2e2b542bf54a581061f9a5757
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-01-05 00:08:17 +0600

    Create Array1.cpp

---
+++ Array1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main()
+
+{
+    int mark[]={54, 84, 24, 43, 92};
+
+    for(int i=0;i<5; i++)
+    {
+        cout<<mark[i]<<"  ";
+    }
+
+
+    getch();
+}
