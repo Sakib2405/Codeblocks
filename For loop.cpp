8e89b4d1c2117dbe52ccb76ff96c8b16dd083916
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-01-10 19:30:43 +0600

    Create For loop.cpp

---
+++ For loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+
+{
+    for(int i=1; i<=10; i++)
+    {
+        cout<<setw (4) <<i<<")" <<" Bangladesh"<<endl;
+    }
+
+    getch();
+}
