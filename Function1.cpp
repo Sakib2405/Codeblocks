8ff514b3dd878b3c6990ed63ed413df928862d01
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-01-16 00:12:45 +0600

    Create Function1.cpp

---
+++ Function1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void addition(int, int);
+
+int main()
+{
+     addition(10,20);
+
+
+    getch();
+}
+
+void addition(int a, int b)
+    {
+   int sum=a+b;
+     cout<<"The sum is: "<<sum;
+    }
