147b46dd2fb004971e8797004294f22e654869af
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-01-21 10:25:07 +0600

    Create Logic.cpp

---
+++ Logic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+    int num1, num2, max;
+
+    cout<<"Enter two number: ";
+    cin>>num1 >>num2;
+
+    max=(num1>num2)? num1:num2;
+
+    cout<<max<<" is Max.";
+
+
+    getch();
+}
