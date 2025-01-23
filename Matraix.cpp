3712dfddc2d375b97c3aa4a52b28fe57287a9f4b
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-01-24 02:27:14 +0600

    Create Matraix.cpp

---
+++ Matraix.cpp
@@ -0,0 +1,81 @@
+/*
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+    int A[3][3]={
+                        {23,34,24},
+                        {24,13, 10},
+                        {12,24,94}
+                       };
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                cout<<A[i][j]<<"  ";
+            }
+            cout<<endl;
+        }
+
+    getch();
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int A [2][3]=
+       {
+        {23,42,21},
+        {34,22,45}
+        };
+
+        for(int i=0; i<2; i++)
+        {
+            for(int j=0; j<3;j++)
+            {
+                cout<<A[i][j]<<"  ";
+            }
+            cout<<endl;
+        }
+
+
+
+
+
+
+
+
+    getch();
+}
