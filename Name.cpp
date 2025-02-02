c2462795be0798a4fb265ecd9fa398955c005641
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-02-02 14:51:08 +0600

    Create Name.cpp

---
+++ Name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main ()
+{
+
+    cout<< "\t Nazmus Sakib\n " << endl <<"\t Barishal\n" <<endl <<"\t01741976660\n";
+    getch();
+}
