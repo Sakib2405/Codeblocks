b393bfe197c71e6522f7580e52bc5ba8bd160543
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-02-09 00:48:49 +0600

    Create Practice.cpp

---
+++ Practice.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the total students: ";
+    cin>> n;
+    int students[n],sum=0,marks[5];
+
+    for(int i=1; i<=n; i++)
+    char name [50] ;
+    cout<<"No. "<<i <<" student name is: ";
+    cin>>name;
+    cout<<endl<<endl;
+    cout<<name <<"'s" <<" mark is: "<<endl<<endl;
+cout<<endl;
+
+    for(int i=1; i<=5; i++)
+    {
+        cout<<"Mark of sub "<<i<<" is: ";
+        cin>>marks[5];
+    }
+}
+
+getch();
+}
