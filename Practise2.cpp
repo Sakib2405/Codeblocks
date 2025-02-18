b4926f65e3bbbffe608753709846002cfe201f09
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-02-19 02:07:53 +0600

    Create Practise2.cpp

---
+++ Practise2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cout<<"Enter the number of students: ";
+   cin>>n;
+
+    int marks[5];
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            cin>>marks[5];
+            cout<<"Sub "<<i<<"mark is: ";
+        }
+    }
+
+    getch();
+}
