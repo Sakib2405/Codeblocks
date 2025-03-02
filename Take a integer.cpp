28be13add516f912c71f1691a1611e8b1ee25c13
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-03-02 07:56:28 +0600

    Create Take a integer.cpp

---
+++ Take a integer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+    int num1, num2,sum;
+    cout<< "Enter two number: ";
+    cin>> num1>>num2;
+
+    cout<<"Entered Number is: " <<num1 <<"  " <<  num2;
+    sum=num1+num2;
+    cout<<"\n\n" << "The sum is: " << sum;
+
+    getch();
+}
