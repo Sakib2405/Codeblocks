6e34e6c799fe7a81569c946ebebbf533123f3c87
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2025-03-08 01:48:11 +0600

    Create Variable , Data tyoe.cpp

---
+++ Variable , Data tyoe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main()
+{
+    int num1=10, num2=20;
+    char ch='a';
+    cout<<"Number 1= "  <<num1 <<endl <<"Number 2= "  <<num2 <<endl;
+    cout<<"Character = " << ch;
+    getch();
+
+
+}
